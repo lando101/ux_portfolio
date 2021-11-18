4db41be2e6de9853776fbd3473b6951c098da7fe
landon.c.messmer <messmer_landon@bah.com>
2021-11-17 20:22:44 -0500

    updates

--- src/app/pages/pas/pas.module.ts
+++ src/app/pages/pas/pas.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PasRoutingModule } from './pas-routing.module';
 import { PasComponent } from './pas.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [PasComponent],
-  imports: [CommonModule, PasRoutingModule, FlexLayoutModule],
+  imports: [CommonModule, PasRoutingModule, FlexLayoutModule, NgbModule],
 })
 export class PasModule {}
