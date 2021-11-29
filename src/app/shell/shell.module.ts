359f7b46b67ecea2ad948af349722748b1dda554
landon.c.messmer <messmer_landon@bah.com>
2021-11-29 15:24:04 -0500

    updates

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -8,9 +8,10 @@ import { I18nModule } from '@app/i18n';
 import { ShellComponent } from './shell.component';
 import { HeaderComponent } from './header/header.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, TranslateModule, NgbModule, I18nModule, RouterModule, FlexLayoutModule],
-  declarations: [HeaderComponent, ShellComponent],
+  declarations: [HeaderComponent, ShellComponent, FooterComponent],
 })
 export class ShellModule {}
