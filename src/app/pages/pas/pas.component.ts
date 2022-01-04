ebde7013a52d26a29c2ca54323d9c79d5e3e0d55
landon.c.messmer <messmer_landon@bah.com>
2022-01-04 13:07:08 -0500

    updates

--- src/app/pages/pas/pas.component.ts
+++ src/app/pages/pas/pas.component.ts
@@ -13,8 +13,8 @@ import {
 export class PasComponent implements OnInit {
   // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   images = [
-    'https://firebasestorage.googleapis.com/v0/b/ux-portfolio-8fc64.appspot.com/o/website_images%2FTech_Savvy.png?alt=media&token=0cc0af7d-8591-4970-9b3c-4b2707c5dd1a',
-    'https://firebasestorage.googleapis.com/v0/b/ux-portfolio-8fc64.appspot.com/o/website_images%2FOrg_Expert.png?alt=media&token=77b50d1c-51ac-4bf1-a6ab-c196fa340adb',
+    'https://firebasestorage.googleapis.com/v0/b/ux-portfolio-8fc64.appspot.com/o/website_images%2FTech_Savvy.png?alt=media&token=50ad5d91-9281-4e77-9567-f3b64fae5c9c',
+    'https://firebasestorage.googleapis.com/v0/b/ux-portfolio-8fc64.appspot.com/o/website_images%2FOrg_Expert.png?alt=media&token=a2b76579-a004-404e-a5e1-217654329e61',
   ];
 
   constructor() {}
