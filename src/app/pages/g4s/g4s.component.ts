f118a25ca0fb72e7ee663bb3ffd4c413f6b3e4a6
landon.c.messmer <messmer_landon@bah.com>
2022-03-03 14:44:45 -0500

    updates to g4s page

--- src/app/pages/g4s/g4s.component.ts
+++ src/app/pages/g4s/g4s.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-g4s',
@@ -6,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./g4s.component.scss'],
 })
 export class G4sComponent implements OnInit {
-  constructor() {}
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  userFlows = [
+    'https://ux-portfolio-lcm.s3.amazonaws.com/g4s/user-flows/Create+Citation+Export+(1).png',
+    'https://ux-portfolio-lcm.s3.amazonaws.com/g4s/user-flows/Request+New+Source+Export.png',
+    'https://ux-portfolio-lcm.s3.amazonaws.com/g4s/user-flows/View+Source+Credibility+Export.png',
+  ];
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 20000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
 
   ngOnInit(): void {}
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
 }
